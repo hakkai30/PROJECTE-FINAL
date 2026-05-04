ee96ee6dfaa45ad8dc4e0340c6a72c5000686800
hakkai30 <robsgm30@gmail.com>
2026-05-04 17:45:01 +0200

    Configuración de Vite: eliminar avisos de chunk size

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,17 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return "vendor";
+          }
+        },
+      },
+    },
+  },
 });
