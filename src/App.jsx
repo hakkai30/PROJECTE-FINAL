9c4bc7933e0545699b7246ec9a5eb24a0b1947b4
hakkai30 <robsgm30@gmail.com>
2026-05-08 13:46:18 +0200

    Fix: Corrected prop name in SocialFeedPage to fix like functionality

--- src/App.jsx
+++ src/App.jsx
@@ -320,7 +320,7 @@ const App = () => {
           } />
           <Route path="/product/:id" element={<ProductDetailPage {...commonProps} product={selectedProduct} addToCart={addToCart} wishlistIds={wishlistIds} onToggleWishlist={toggleWishlist} />} />
           <Route path="/social" element={
-            <SocialFeedPage {...commonProps} posts={socialPosts} isLoadingPosts={isLoadingSocialPosts} feedError={socialFeedError} activeView={socialFeedFilter} onViewChange={setSocialFeedFilter} savedLookIds={savedLookIds} onToggleSavedLook={toggleSavedLook} onLikePost={toggleSocialLike} onAddComment={addSocialComment} loadMorePosts={() => loadSocialPosts(true)} refreshPosts={() => loadSocialPosts(false)} onCreatePost={createSocialPost} onOpenProfile={openProfile} />
+            <SocialFeedPage {...commonProps} posts={socialPosts} isLoadingPosts={isLoadingSocialPosts} feedError={socialFeedError} activeView={socialFeedFilter} onViewChange={setSocialFeedFilter} savedLookIds={savedLookIds} onToggleSavedLook={toggleSavedLook} onToggleLikePost={toggleSocialLike} onAddComment={addSocialComment} loadMorePosts={() => loadSocialPosts(true)} refreshPosts={() => loadSocialPosts(false)} onCreatePost={createSocialPost} onOpenProfile={openProfile} />
           } />
           <Route path="/social/saved" element={<SavedLooksPage {...commonProps} posts={socialPosts} savedLookIds={savedLookIds} onToggleSavedLook={toggleSavedLook} />} />
           <Route path="/profile" element={
